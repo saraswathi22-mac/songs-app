51dc00fd465b34a76b55bc0f528eae32acbc7766
exit <saraswathi.mac@gmail.com>
2023-02-16 15:17:15 +0530

    Updated changes for routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import Sidebar from "./components/Sidebar/Sidebar.js";
+import Discover from "./pages/Discover.js";
 import Home from "./pages/Home.jsx";
 
 const App = () => {
   return (
+    <section className="flex gap-6">
+      <Sidebar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/discover" element={<Discover />} />
       </Routes>
+    </section>
   );
 };
 
