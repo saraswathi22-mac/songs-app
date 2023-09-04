d119b844b79d735ea0b114c4fadea1164a3934ee
exit <saraswathi.mac@gmail.com>
2023-09-04 11:06:04 +0530

    Updated Songs app

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      TunLy
+    </>
+  )
+}
+
+export default App
--- src/pages/Home.tsx
+++
@@ -1,9 +0,0 @@
-const Home = () => {
-  return (
-    <div className="m-3 text-xl text-gray-900 font-semibold">
-      REACT TAILWIND
-    </div>
-  );
-};
-
-export default Home;
