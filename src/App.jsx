b5df21539f16436f0a913e02e962bdc77f8c2a7a
exit <saraswathi.mac@gmail.com>
2024-02-15 16:34:24 +0530

    Added song elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import NavBar from './components/Navbar/Navbar'
+import Home from './pages/Home/Home'
 
 function App() {
 
   return (
-    <div>
+    <>
       <NavBar />
-    </div>
+      <Home />
+    </>
   )
 }
 
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,34 @@
+import "./Home.css";
+import { useState, useEffect } from "react";
+import { Player } from "../../components/Music/Player";
+import musicData from "../../music";
+
+const Home = () => {
+  const [songs] = useState(musicData);
+
+  const [currentSongIndex, setCurrentSongIndex] = useState(0);
+  const [nextSongIndex, setNextSongIndex] = useState(0);
+
+  useEffect(() => {
+    setNextSongIndex(() => {
+      if (currentSongIndex + 1 > songs.length - 1) {
+        return 0;
+      } else {
+        return currentSongIndex + 1;
+      }
+    });
+  }, [currentSongIndex, songs.length]);
+
+  return (
+    <div className="App">
+      <Player
+        currentSongIndex={currentSongIndex}
+        setCurrentSongIndex={setCurrentSongIndex}
+        nextSongIndex={nextSongIndex}
+        songs={songs}
+      />
+    </div>
+  );
+};
+
+export default Home;
